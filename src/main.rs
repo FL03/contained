15043483e8ce003aeda8b7426c70defd8bb7b195
Joe McCain III <jo3mccain@icloud.com>
2022-12-18 15:41:25 +0000

    Update

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ pub(crate) mod settings;
 pub(crate) mod utils;
 
 pub mod cli;
+pub mod runtime;
 pub mod states;
 
 use scsys::prelude::{BoxResult, Message};
---
+++ src/runtime.rs
@@ -0,0 +1,19 @@
+/*
+    Appellation: runtime <module>
+    Contrib: FL03 <jo3mccain@icloud.com>
+    Description: ... summary ...
+*/
+use crate::states::States;
+use std::sync::{Arc, Mutex};
+use tokio::sync::mpsc::Receiver;
+
+#[derive(Debug)]
+pub struct Runtime {
+    pub(crate) state: Receiver<Arc<States>>
+}
+
+impl Runtime {
+    pub fn new(state: Receiver<Arc<States>>) -> Self {
+        Self { state }
+    }
+}
