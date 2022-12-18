5814301a4887febeae96dbe257cf43566a312587
Joe McCain III <jo3mccain@icloud.com>
2022-12-18 18:02:21 +0000

    Update

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -3,15 +3,13 @@
     Contrib: FL03 <jo3mccain@icloud.com>
     Description: ... Summary ...
 */
-use clap::Parser;
-use scsys_xtask::cmd;
 
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     tracing::info!("Welcome to xtask...");
 
     let handle = std::thread::spawn(move || {
-        scsys_xtask::cli::handle().join().unwrap();
+        xtask::cli::handle().join().unwrap();
     });
     handle.join().ok().unwrap();
 
