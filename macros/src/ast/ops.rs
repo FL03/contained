3f5b353d260c5dfde8c793742628897a4e83f76f
FL03 <jo3mccain@icloud.com>
2025-12-18 08:04:27 -0600

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- macros/src/ast/ops.rs
+++ macros/src/ast/ops.rs
@@ -2,22 +2,22 @@
     appellation: ops <module>
     authors: @FL03
 */
+use crate::ast::WrapperOpsAst;
+
 use syn::parse::{Parse, ParseStream};
 use syn::token::Impl;
 use syn::{Ident, Token, braced};
 
-/// The abstract syntax tree for the `binary_wrapper` macro input;
-/// e.g. `impl A { Add.add, Sub.sub }` or `impl B.field { Add.add, Sub.sub }`
-pub struct WrapperOpsAst {
-    pub _impl: Impl,
-    pub target: Ident,
-    pub field: Option<Ident>,
-    pub ops: Vec<(Ident, Ident)>,
-}
-
 impl Parse for WrapperOpsAst {
     fn parse(input: ParseStream) -> syn::Result<Self> {
+        // parse the `impl` keyword
         let _impl = input.parse::<Impl>()?;
+        // detect any generic parameters
+        let generics = if input.peek(Token![<]) {
+            input.parse().ok()
+        } else {
+            None
+        };
         let target = input.parse::<Ident>()?;
         // resolve the optional named field
         let field = if input.peek(Token![.]) {
@@ -26,6 +26,13 @@ impl Parse for WrapperOpsAst {
         } else {
             None
         };
+        // parse the optional where clause
+        let where_clause = if input.peek(Token![where]) {
+            Some(input.parse()?)
+        } else {
+            None
+        };
+        // parse the operations defined within braces
         let content;
         braced!(content in input);
         let mut ops = Vec::new();
@@ -40,8 +47,10 @@ impl Parse for WrapperOpsAst {
         }
         Ok(Self {
             _impl,
+            generics,
             target,
             field,
+            where_clause,
             ops,
         })
     }
--- macros/src/ast/wrapper.rs
+++ macros/src/ast/wrapper.rs
@@ -2,22 +2,11 @@
     appellation: wrapper <module>
     authors: @FL03
 */
+use crate::ast::{NamedFieldAst, WrapperAst};
+
 use syn::parse::{Parse, ParseStream};
 use syn::{Ident, Token};
 
-pub struct WrapperAst {
-    pub target: Ident,
-    pub field: Option<NamedFieldAst>,
-}
-
-pub struct NamedFieldAst {
-    pub period: Token![.],
-    pub field: Ident,
-}
-
-/*
- ************* Implementations *************
-*/
 impl Parse for NamedFieldAst {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         let period = input.parse::<Token![.]>()?;
