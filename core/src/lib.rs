4538bd59119a9ae15b63b9f484ebf445d194501f
FL03 <jo3mccain@icloud.com>
2025-12-26 20:54:52 -0600

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -4,21 +4,22 @@
 */
 //! this core components of the contained crate
 #![allow(
+    clippy::missing_docs_in_private_items,
+    clippy::missing_errors_doc,
     clippy::missing_safety_doc,
     clippy::module_inception,
     clippy::needless_doctest_main,
-    clippy::upper_case_acronyms
+    clippy::upper_case_acronyms,
 )]
 #![cfg_attr(not(feature = "std"), no_std)]
-
+#![cfg_attr(feature = "nightly", feature(allocator_api))]
+// compile-time checks
 #[cfg(not(any(feature = "std", feature = "alloc")))]
-compile_error! {
-    "Either the 'std' or 'alloc' feature must be enabled."
-}
-
+compile_error! { "Either the 'std' or 'alloc' feature must be enabled." }
+// external crates
 #[cfg(feature = "alloc")]
 extern crate alloc;
-
+// macros
 #[macro_use]
 pub(crate) mod macros {
     #[macro_use]
@@ -33,16 +34,17 @@ pub(crate) mod macros {
         pub mod wrapper;
     }
 }
-
+// modules
 pub mod error;
+
+pub mod traits {
+    //! core traits and interfaces for wrappers and their operations, formatting, etc.
+}
 // re-exports
 #[doc(inline)]
 pub use self::{
     error::{Error, Result},
-    traits::prelude::*,
 };
-#[doc(inline)]
-pub use contained_traits as traits;
 // prelude
 #[doc(hidden)]
 pub mod prelude {
