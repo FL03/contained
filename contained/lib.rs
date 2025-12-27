6d83e471c62809693f9e17661396283e7768a878
FL03 <jo3mccain@icloud.com>
2025-12-26 22:28:57 -0600

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- contained/lib.rs
+++ contained/lib.rs
@@ -1,32 +1,33 @@
-/*
-    Appellation: contained <library>
-    Contrib: FL03 <jo3mccain@icloud.com>
-*/
-//! # contained
-//!
-//! Welcome to `contained`! A library focused on providing useful abstractions, macros, and
-//! utilities for handling so-called wrapper types. In short, a wrapper type is any implemented
-//! object capable of using `#[repr(transparent)]`.
+//! `contained` is a collection of macros, traits, and other useful primitives for defining,
+//! managing, and interacting with so-called wrapper types. More specifically, any type capable
+//! of deriving the `#[repr(transparent)]` attribute in Rust.
 #![allow(
+    clippy::missing_docs_in_private_items,
+    clippy::missing_errors_doc,
     clippy::missing_safety_doc,
     clippy::module_inception,
     clippy::needless_doctest_main,
     clippy::upper_case_acronyms
 )]
 #![cfg_attr(not(feature = "std"), no_std)]
-
-#[cfg(any(feature = "alloc", feature = "std"))]
+#![cfg_attr(feature = "nightly", feature(allocator_api))]
+// compile-time checks 
+#[cfg(not(any(feature = "alloc", feature = "std")))]
+compile_error! { "Either the 'alloc' or 'std' feature must be enabled for this crate to compile." }
+// external crates
+#[cfg(feature = "alloc")]
+extern crate alloc;
+// re-exports
 pub use contained_core::*;
-
 #[cfg(feature = "derive")]
 pub use contained_derive::*;
 #[cfg(feature = "macros")]
 pub use contained_macros::*;
-
-#[allow(unused_imports)]
+// prelude
+#[doc(hidden)]
 pub mod prelude {
-    #[cfg(any(feature = "alloc", feature = "std"))]
-    pub use contained_core::prelude::*;
     #[cfg(feature = "derive")]
     pub use contained_derive::*;
+    #[cfg(feature = "macros")]
+    pub use contained_macros::*;
 }
